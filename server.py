876c9a14e1d95440a3e6d0389e8722e95a2ccb1f
Kushagra Srivastava <kushagrasrivas1930@gmail.com>
2021-10-13 03:59:40 +0530

    For Base Prototy From Frontend to Backend DONEgit statusgit status

--- server.py
+++ server.py
@@ -1,27 +1,33 @@
 from fastapi import FastAPI, Request, UploadFile, status
 from fastapi.params import File
 from fastapi.datastructures import UploadFile
-from fastapi.templating import Jinja2Templates 
+from fastapi.templating import Jinja2Templates
+from main import prototype 
 from starlette.requests import Request
 from fastapi.responses import FileResponse
 import shutil
 import os
-import ocr
-import requests
-from pydantic import BaseModel
-from typing import List
-from pymongo import MongoClient
-import motor.motor_asyncio
+# import ocr
+# import requests
+# from pydantic import BaseModel
+# from typing import List
+# from pymongo import MongoClient
+# import motor.motor_asyncio
 import logging
+from fastapi.staticfiles import StaticFiles
 
-mongo_url = "mongodb+srv://admin-kushagra:kushagra@cluster0.imgaq.mongodb.net/TextDB?retryWrites=true&w=majority"
-client = motor.motor_asyncio.AsyncIOMotorClient(mongo_url)
-db = client["TextDB"]
-collection = db["Files"]
+
+# mongo_url = "mongodb+srv://admin-kushagra:kushagra@cluster0.imgaq.mongodb.net/TextDB?retryWrites=true&w=majority"
+# client = motor.motor_asyncio.AsyncIOMotorClient(mongo_url)
+# db = client["TextDB"]
+# collection = db["Files"]
 
 path = "C:/Users/OneDrive/Kushagra Srivastava/Desktop/pymongo-1"
 
 app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
 
 logger = logging.getLogger("app")
 logger.setLevel(logging.DEBUG)
@@ -35,8 +41,10 @@ def home(request: Request):
 @app.post('/upload')
 def perform_ocr(file: UploadFile = File(...)):
     temp_file = _save_file_to_disk(file, path="temp", save_as="temp_file")
-    text = ocr.pdfToTxt(temp_file)
-    return {"filename": file.filename, "text": text}
+    prototype()
+    return temp_file
+    # text = ocr.pdfToTxt(temp_file)
+    # return {"filename": file.filename, "text": text}
     
 
 def _save_file_to_disk(uploaded_file, path=".", save_as="default"):
@@ -48,7 +56,7 @@ def _save_file_to_disk(uploaded_file, path=".", save_as="default"):
 
 @app.get('/download')
 def download_file():
-    return FileResponse("temp/out_text.xlsx")
+    return FileResponse("Data_Extraction01.xlsx")
 
     
     
